06e5fc7bd2098aeca9a0d751b8ac22e533dfe2a7
“mayluv17” <mayowaibrahim009@gmail.com>
2023-10-29 18:31:02 +0100

    setting ts

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,28 @@
+import arrowRight from "@/assets/icons";
+
+import { chevronLeft } from "@/assets/icons";
+import { cheveronRight } from "@/assets/icons";
+import { copyrightSign } from "@/assets/copyright-sign.svg";
+import { facebook } from "@/assets/facebook.svg";
+import { hamburger } from "@/assets/hamburger.svg";
+import instagram from "@/assets/instagram.svg";
+import support from "@/assets/support.svg";
+import shieldTick from "@/assets/shield-tick.svg";
+import star from "@/assets/star.svg";
+import truckFast from "@assets/truck-fast.svg";
+import twitter from ".";
+
+export {
+  arrowRight,
+  cheveronRight,
+  chevronLeft,
+  copyrightSign,
+  facebook,
+  hamburger,
+  instagram,
+  support,
+  shieldTick,
+  star,
+  truckFast,
+  twitter,
+};
