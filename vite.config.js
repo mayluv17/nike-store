91e9d314c21ddc91197e8b688f44a5c1646e02b4
“mayluv17” <mayowaibrahim009@gmail.com>
2023-11-17 11:38:33 +0100

    configuring build base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/<REPO>/",
+  base: "/<nike-store>/",
   plugins: [react()],
   server: {
     host: "0.0.0.0", // Expose the server to all network interfaces
